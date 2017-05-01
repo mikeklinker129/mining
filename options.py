a6b52edffb74b9909fa6e17ba5d23a9a5e1e3277
mklinker <michael.klinker@ll.mit.edu>
2017-05-01 13:13:40 -0400

    asdf

--- options.py
+++ options.py
@@ -5,6 +5,7 @@ import pprint
 import matplotlib.pyplot as plt
 import numpy as np 
 import time
+import sys
 #print F.expiry_dates,'\n'
 
 
@@ -33,34 +34,6 @@ def put_intrinsic(stock,strike):
 
 
 
-
-def calc_iron_condor(sellPut,buyPut,sellCall,buyCall, d1, d2, d3, d4):
-
-    # sellPut > buyPut
-    # sellCall< buyCall
-
-    calc = {}
-    info = {}
-    net_premium = d1 - d2 + d3 - d4
-
-    info['strike_low']  = buyPut
-    info['strike_mid1'] = sellPut
-    info['strike_mid2'] = sellCall   
-    info['strike_high'] = buyCall
-    info['price_low']   = d1
-    info['price_mid1']  = d2
-    info['price_mid2']  = d3
-    info['price_high']  = d4
-
-    calc['max_profit'] = net_premium 
-    calc['low_break']  = d1-net_premium
-    calc['high_break'] = d3+net_premium
-    calc['min_profit'] = d4-sellCall-net_premium
-    calc['width'] = calc['high_break']-calc['low_break']
-
-    rtrn = {'info':info, 'calc':calc}
-    return rtrn
-
 def calc_butterfly_call(s_low, s_mid, s_high, d_low, d_mid, d_high, d_expire):
     calc = {}
     info = {}
@@ -87,6 +60,87 @@ def calc_butterfly_call(s_low, s_mid, s_high, d_low, d_mid, d_high, d_expire):
 
     rtrn = {'info':info, 'calc':calc}
     return rtrn
+
+def calc_iron_condor(buyPut,sellPut,sellCall,buyCall, d1, d2, d3, d4):
+
+    # sellPut > buyPut
+    # sellCall< buyCall
+
+    calc = {}
+    info = {}
+    net_premium = -d1 + d2 + d3 - d4
+
+
+    info['strike_low']  = buyPut
+    info['strike_mid1'] = sellPut
+    info['strike_mid2'] = sellCall   
+    info['strike_high'] = buyCall
+    info['price_low']   = d1
+    info['price_mid1']  = d2
+    info['price_mid2']  = d3
+    info['price_high']  = d4
+
+    calc['max_profit'] = net_premium 
+    calc['low_break']  = d2-net_premium
+    calc['high_break'] = d3+net_premium
+    calc['min_profit'] = -np.max( [buyCall-sellCall-net_premium , sellPut-buyPut-net_premium])
+    calc['width'] = calc['high_break']-calc['low_break']
+
+    rtrn = {'info':info, 'calc':calc}
+
+    # print '\nPrices: -%.2f  %.2f   %.2f  -%.2f '%(d1,d2,d3,d4)
+    # print 'strikes: %.2f   %.2f   %.2f    %.2f' %(buyPut,sellPut,sellCall,buyCall)
+    # print 'net premium: %.1f, max loss %.1f' %(net_premium,calc['min_profit'])
+
+    return rtrn
+
+def loop_iron_condor(calls,puts):
+
+    OTM_calls = []
+    OTM_puts = []
+
+    for i in range(0,len(calls)):
+        if not calls[i]['inTheMoney']:
+            OTM_calls.append(calls[i])
+
+    for i in range(0,len(puts)):
+        if not puts[i]['inTheMoney']:
+            OTM_puts.append(puts[i])
+
+    put_pairs = []
+    for i in range(0,len(OTM_puts)-1):
+        for j in range( i+1,len(OTM_puts) ):
+            put_pairs.append( [OTM_puts[i],OTM_puts[j]] ) #low strike, high strike
+
+    call_pairs = []
+    for i in range(0,len(OTM_calls)-1):
+        for j in range(i+1,len(OTM_calls)):
+            call_pairs.append( [OTM_calls[i] , OTM_calls[j]] )
+
+    condors = []
+
+    for pairP in put_pairs:
+        for pairC in call_pairs:
+            s_low = pairP[0]['strike']
+            s_mid1 = pairP[1]['strike']
+            s_mid2 = pairC[0]['strike']
+            s_high = pairC[1]['strike']
+
+            p_low = pairP[0]['lastPrice']
+            p_mid1 = pairP[1]['lastPrice']
+            p_mid2 = pairC[0]['lastPrice']
+            p_high = pairC[1]['lastPrice']
+            
+            if  abs(abs(s_mid1-s_low) - abs(s_high-s_mid2))<1:
+
+                condors.append(calc_iron_condor(s_low,s_mid1,s_mid2,s_high, p_low,p_mid1,p_mid2,p_high))
+
+
+
+
+    condors.sort(key=lambda r: -r['calc']['min_profit'])
+
+    return condors
 
 def loop_butterfly_call(calls,ex_date):
     # Find Center call to sell.
@@ -173,8 +227,11 @@ def plot_butterfly(sp):
     plt.show()
 
 
-symb = 'EUO'
+####################################################################
+
+symb = 'IBM'
 quote = web.get_quote_yahoo(symb)
+price = quote.iloc[0]['last']
 option_m = Options(symb,'yahoo')
 print option_m.expiry_dates
 ex_date = option_m.expiry_dates[1]
@@ -200,6 +257,69 @@ for i in range(0,100):#data.iloc:
         break
     except Exception as e:
         break
+
+
+results = loop_iron_condor(calls,puts)
+
+print 'N: ',len(results)
+
+for i in range(0,len(results)):
+    a= results[i]['info']
+    b= results[i]['calc']
+
+# print len(a)
+    #print '\nPrices: -%.2f  %.2f   %.2f  -%.2f '%()
+    min_prof = results[i]['calc']['min_profit']
+    max_prof = results[i]['calc']['max_profit']
+
+    spread = b['width']/price*100
+
+
+
+    if spread>.1 and max_prof>.1 and min_prof>-5:
+
+
+        print '\nMax Profit: %.1f    Max loss: %.1f' %(max_prof,min_prof)
+        print 'strikes: %.2f   %.2f   %.2f    %.2f' %(a['strike_low'],a['strike_mid1'],a['strike_mid2'],a['strike_high'])
+        print 'Spread: %.2f  percentage:  %.1f ' %(b['width'], b['width']/price*100 )
+
+
+
+    #print ,,results[i]['calc']['max_profit']
+
+
+sys.exit(1)
+##########################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 price = quote.iloc[0]['last']
 y_all = []
